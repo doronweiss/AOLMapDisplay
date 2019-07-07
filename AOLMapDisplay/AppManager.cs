e5ac0c2437affe69ab284d42893daf6b38dae18d
doron <doron@10fs.co.il>
2019-07-07 18:25:00 +0300

    nearly loading

--- AOLMapDisplay/AppManager.cs
+++ AOLMapDisplay/AppManager.cs
@@ -9,22 +9,13 @@ namespace AOLMapDisplay {
     // feature keys
     const string KeyField = "ID";
     static int featuresKey = 0;
+    MappedDataStore mappedData = new MappedDataStore();
 
     public AppManager() {
     }
 
     ~AppManager() {
     }
-
-    internal enum InitRes { Bad, Ok, NeedsCfg };
-    public InitRes Init(out string errorStr) {
-      errorStr = "";
-      return InitRes.Ok;
-    }
-
-    public void UnInit() {
-    }
-
 
     #region implement CoordinatesConverter
     public void ToWorld(List<GeoLocation> geo, ref List<WorldCoord> world) {
@@ -52,137 +43,145 @@ namespace AOLMapDisplay {
       trjLyrFeatures.Clear();
     }
 
-/*
-    // draw a trajectory - simple version called on change
-    private void DrawTrajectory(MappedLine traj, TrajChangeOpEnum changeType) {
-      DrawTrajectory(traj, changeType, false);
-    }
+    public bool LoadData(string fileName, bool clearFirst) {
+      if (!mappedData.LoadData(fileName, clearFirst))
+        return false;
 
-    // draws the trajectory with waypo
-    public void DrawWithWP(MappedLine traj) {
-      DrawTrajectory(traj, TrajChangeOpEnum.Changed, true);
+      return true;
     }
 
 
-    private void DrawTrajectory(MappedLine traj, TrajChangeOpEnum changeType, bool drawWaypoints) {
-      // set style
-      bool applyStyle = traj.Selected || traj.HasWaypoints;
-      Mapsui.Styles.Color styleColor = Mapsui.Styles.Color.Black;
-      if (applyStyle) {
-        if (traj.Selected) {
-          if (traj.HasWaypoints)
-            styleColor = Mapsui.Styles.Color.Yellow;
-          else
-            styleColor = Mapsui.Styles.Color.Violet;
-        } else {
-          if (traj.HasWaypoints)
-            styleColor = Mapsui.Styles.Color.Green;
+    /*
+        // draw a trajectory - simple version called on change
+        private void DrawTrajectory(MappedLine traj, TrajChangeOpEnum changeType) {
+          DrawTrajectory(traj, changeType, false);
         }
-      }
-      // draw
-      Mapsui.Providers.Feature ft = null;
-      // clear current
-      foreach (object o in traj.mapFeatures) // traj features
-        trjLyrFeatures.Delete(o);
-      traj.mapFeatures.Clear();
-      foreach (object o in traj.waypointsFeatures) // mislul features
-        trjLyrFeatures.Delete(o);
-      traj.waypointsFeatures.Clear();
-      switch (changeType) {
-        case TrajChangeOpEnum.Changed:
-          #region draw vertices
-          for (int idx = 0; idx < traj.verticesW.Count; idx++) {
-            WorldCoord w = traj.verticesW[idx];
-            ft = new Mapsui.Providers.Feature {
-              Geometry =
-                new Mapsui.Geometries.Point() {
-                  X = w.x, Y = w.y
-                },
-              ["Label"] = $"Vertex {idx}",
-              ["Type"] = "Traj vertex"
-            };
-            if (traj.Selected && idx == traj.SelectedVIdx) {
-              ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
-                Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Green),
-                Outline = { Color = Mapsui.Styles.Color.Red, Width = 2 }
-              });
-            } else {
-              ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
-                SymbolScale = idx == 0 ? 1.1 : 0.8,
-                Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Gray),
-                Outline = { Color = Mapsui.Styles.Color.Black, Width = 2 }
-              });
-            }
-            ft[KeyField] = featuresKey;
-            trjLyrFeatures.Add(ft);
-            traj.mapFeatures.Add(featuresKey++);
-          }
-          #endregion draw vertices
-          #region draw line string - traj
-          if (traj.verticesW.Count > 1) {
-            ft = new Mapsui.Providers.Feature {
-              Geometry =
-                new Mapsui.Geometries.LineString() {
-                  Vertices = traj.verticesW.Select(x => new Mapsui.Geometries.Point() { X = x.x, Y = x.y }).ToList() as List<Mapsui.Geometries.Point>
-                }
-            };
-            ft[KeyField] = featuresKey;
-            if (applyStyle)
-              ft.Styles.Add(new Mapsui.Styles.VectorStyle() { Line = new Pen(styleColor, 2) });
-            trjLyrFeatures.Add(ft);
-            traj.mapFeatures.Add(featuresKey++);
-          }
-          #endregion draw line string - traj
-          #region draw waypoints
-          if (drawWaypoints && traj.HasWaypoints && traj.wayPoints.Count > 1) {
-            ft = new Mapsui.Providers.Feature {
-              Geometry =
-                new Mapsui.Geometries.LineString() {
-                  Vertices = traj.wayPoints.Select(x => MapsuiPtFromWaypoint(x)).ToList() as List<Mapsui.Geometries.Point>
-                },
-              ["Label"] = $"Error waypoint",
-              ["Type"] = ""
-            };
-            ft[KeyField] = featuresKey;
-            ft.Styles.Add(new Mapsui.Styles.VectorStyle() { Line = new Pen(Mapsui.Styles.Color.Red, 2) });
-            trjLyrFeatures.Add(ft);
-            traj.waypointsFeatures.Add(featuresKey++);
-          }
-          #endregion draw waypoints
-          #region draw error locations
-          if (drawWaypoints && traj.HasErrorLocs) {
-            for (int idx = 0; idx < traj.errorLocs.Count; idx++) {
-              WorldCoord w = FromGeo(traj.errorLocs[idx]);
-              ft = new Mapsui.Providers.Feature {
-                Geometry =
-                  new Mapsui.Geometries.Point() {
-                    X = w.x, Y = w.y
-                  },
-                ["Label"] = traj.errorLocs[idx].description,
-                ["Type"] = ""
-              };
-              if (traj.Selected && idx == traj.SelectedVIdx)
-                ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
-                  SymbolScale = 0.8,
-                  Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Red),
-                  Outline = { Color = Mapsui.Styles.Color.Red, Width = 5 }
-                });
-              ft[KeyField] = featuresKey;
-              trjLyrFeatures.Add(ft);
-              traj.waypointsFeatures.Add(featuresKey++);
-            }
-          }
-          #endregion draw error locations
-          break;
-        case TrajChangeOpEnum.Removed:
-          break;
-      }
-    }
-*/
-    #endregion
 
-    #region Coordinates conversion
-    WorldCoord FromGeo(GeoLocation g) {
+        // draws the trajectory with waypo
+        public void DrawWithWP(MappedLine traj) {
+          DrawTrajectory(traj, TrajChangeOpEnum.Changed, true);
+        }
+
+
+        private void DrawTrajectory(MappedLine traj, TrajChangeOpEnum changeType, bool drawWaypoints) {
+          // set style
+          bool applyStyle = traj.Selected || traj.HasWaypoints;
+          Mapsui.Styles.Color styleColor = Mapsui.Styles.Color.Black;
+          if (applyStyle) {
+            if (traj.Selected) {
+              if (traj.HasWaypoints)
+                styleColor = Mapsui.Styles.Color.Yellow;
+              else
+                styleColor = Mapsui.Styles.Color.Violet;
+            } else {
+              if (traj.HasWaypoints)
+                styleColor = Mapsui.Styles.Color.Green;
+            }
+          }
+          // draw
+          Mapsui.Providers.Feature ft = null;
+          // clear current
+          foreach (object o in traj.mapFeatures) // traj features
+            trjLyrFeatures.Delete(o);
+          traj.mapFeatures.Clear();
+          foreach (object o in traj.waypointsFeatures) // mislul features
+            trjLyrFeatures.Delete(o);
+          traj.waypointsFeatures.Clear();
+          switch (changeType) {
+            case TrajChangeOpEnum.Changed:
+              #region draw vertices
+              for (int idx = 0; idx < traj.verticesW.Count; idx++) {
+                WorldCoord w = traj.verticesW[idx];
+                ft = new Mapsui.Providers.Feature {
+                  Geometry =
+                    new Mapsui.Geometries.Point() {
+                      X = w.x, Y = w.y
+                    },
+                  ["Label"] = $"Vertex {idx}",
+                  ["Type"] = "Traj vertex"
+                };
+                if (traj.Selected && idx == traj.SelectedVIdx) {
+                  ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
+                    Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Green),
+                    Outline = { Color = Mapsui.Styles.Color.Red, Width = 2 }
+                  });
+                } else {
+                  ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
+                    SymbolScale = idx == 0 ? 1.1 : 0.8,
+                    Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Gray),
+                    Outline = { Color = Mapsui.Styles.Color.Black, Width = 2 }
+                  });
+                }
+                ft[KeyField] = featuresKey;
+                trjLyrFeatures.Add(ft);
+                traj.mapFeatures.Add(featuresKey++);
+              }
+              #endregion draw vertices
+              #region draw line string - traj
+              if (traj.verticesW.Count > 1) {
+                ft = new Mapsui.Providers.Feature {
+                  Geometry =
+                    new Mapsui.Geometries.LineString() {
+                      Vertices = traj.verticesW.Select(x => new Mapsui.Geometries.Point() { X = x.x, Y = x.y }).ToList() as List<Mapsui.Geometries.Point>
+                    }
+                };
+                ft[KeyField] = featuresKey;
+                if (applyStyle)
+                  ft.Styles.Add(new Mapsui.Styles.VectorStyle() { Line = new Pen(styleColor, 2) });
+                trjLyrFeatures.Add(ft);
+                traj.mapFeatures.Add(featuresKey++);
+              }
+              #endregion draw line string - traj
+              #region draw waypoints
+              if (drawWaypoints && traj.HasWaypoints && traj.wayPoints.Count > 1) {
+                ft = new Mapsui.Providers.Feature {
+                  Geometry =
+                    new Mapsui.Geometries.LineString() {
+                      Vertices = traj.wayPoints.Select(x => MapsuiPtFromWaypoint(x)).ToList() as List<Mapsui.Geometries.Point>
+                    },
+                  ["Label"] = $"Error waypoint",
+                  ["Type"] = ""
+                };
+                ft[KeyField] = featuresKey;
+                ft.Styles.Add(new Mapsui.Styles.VectorStyle() { Line = new Pen(Mapsui.Styles.Color.Red, 2) });
+                trjLyrFeatures.Add(ft);
+                traj.waypointsFeatures.Add(featuresKey++);
+              }
+              #endregion draw waypoints
+              #region draw error locations
+              if (drawWaypoints && traj.HasErrorLocs) {
+                for (int idx = 0; idx < traj.errorLocs.Count; idx++) {
+                  WorldCoord w = FromGeo(traj.errorLocs[idx]);
+                  ft = new Mapsui.Providers.Feature {
+                    Geometry =
+                      new Mapsui.Geometries.Point() {
+                        X = w.x, Y = w.y
+                      },
+                    ["Label"] = traj.errorLocs[idx].description,
+                    ["Type"] = ""
+                  };
+                  if (traj.Selected && idx == traj.SelectedVIdx)
+                    ft.Styles.Add(new Mapsui.Styles.SymbolStyle() {
+                      SymbolScale = 0.8,
+                      Fill = new Mapsui.Styles.Brush(Mapsui.Styles.Color.Red),
+                      Outline = { Color = Mapsui.Styles.Color.Red, Width = 5 }
+                    });
+                  ft[KeyField] = featuresKey;
+                  trjLyrFeatures.Add(ft);
+                  traj.waypointsFeatures.Add(featuresKey++);
+                }
+              }
+              #endregion draw error locations
+              break;
+            case TrajChangeOpEnum.Removed:
+              break;
+          }
+        }
+    */
+      #endregion
+
+      #region Coordinates conversion
+      WorldCoord FromGeo(GeoLocation g) {
       return FromGeo(g.lat, g.lon);
     }
 
--- AOLMapDisplay/MappedDataStore.cs
+++ AOLMapDisplay/MappedDataStore.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace AOLMapDisplay {
   class MappedDataStore {
+    private double cntrLat, cntrLon;
     private List<MappedPoint> points;
     private List<MappedLine> lines;
 
@@ -21,6 +22,49 @@ namespace AOLMapDisplay {
           return true;
         default:
           return true;
+      }
+    }
+
+    private bool LoadBaseDrawData(string fileName) {
+      try {
+        string line;
+        using (StreamReader sr = new StreamReader(fileName)) {
+          line = sr.ReadLine();
+          while (line != null) {
+            string[] parts;
+            string typeStr = line.Substring(0, 4);
+            switch (typeStr) {
+              case "cntr":
+                parts = line.Substring(5).Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                cntrLat = double.Parse(parts[0]);
+                cntrLon = double.Parse(parts[1]);
+                break;
+              case "symb": {
+                parts = line.Substring(5).Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                MappedPoint mp = new MappedPoint() { geo = new GeoLocation() { lat = double.Parse(parts[0]), lon = double.Parse(parts[1]), alt = 0.0 } };
+                points.Add(mp);
+              }
+              break;
+              case "line": {
+                parts = line.Substring(5).Split(new[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+                int sz = parts.Length / 2;
+                MappedLine ml = new MappedLine() {vertices = new List<MappedPoint>()};
+                for (int idx = 0; idx < sz; idx++) {
+                  MappedPoint mp = new MappedPoint()
+                    {geo = new GeoLocation() {lat = double.Parse(parts[idx * 2]), lon = double.Parse(parts[idx * 2 + 1]), alt = 0.0}};
+                  ml.vertices.Add(mp);
+                }
+                lines.Add(ml);
+              }
+                break;
+            }
+            line = sr.ReadLine();
+          }
+          return true;
+        }
+      } catch (Exception e) {
+        Console.WriteLine(e);
+        return false; ;
       }
     }
   }
